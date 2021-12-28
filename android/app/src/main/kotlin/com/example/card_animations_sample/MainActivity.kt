8063d84c2f7c176b31a5ab928ee393c04ec70579
runrunhika <71953944+runrunhika@users.noreply.github.com>
2021-12-29 04:43:55 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/card_animations_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_animations_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'ui/ContactListPage.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xff23202a),
+        appBarTheme: AppBarTheme(
+          color: Colors.deepPurple[400],
+          centerTitle: true,
+          iconTheme: const IconThemeData(
+            color: Colors.white70,
+          ),
+        ),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.grey[800]),
+        ),
+        iconTheme: const IconThemeData(color: Colors.grey),
+      ),
+      home: const ContactListPage(),
+    );
+  }
+}
